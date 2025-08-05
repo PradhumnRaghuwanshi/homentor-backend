52e9d83aad9d1adf060af2c961eeafe79f53bed1
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-08-05 13:52:17 +0000

    class record

--- routes/classRecordRoutes.js
+++ routes/classRecordRoutes.js
@@ -21,6 +21,14 @@ router.get("/class-booking/:id", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
+router.put("/:id", async (req, res) => {
+  try {
+    const classRecords = await ClassRecord.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).json({ data: classRecords });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 router.post("/", async (req, res) => {
   try {
     console.log(req.body)
