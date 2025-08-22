3c30085a590354ac86ef658890eb5c3bd9a470ab
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-08-22 06:59:19 +0000

    form

--- models/Mentor.js
+++ models/Mentor.js
@@ -7,11 +7,11 @@ const MentorSchema = new mongoose.Schema({
   },
   phone: {
     type: String,
-    required: true,
+    // required: true,
   },
   gender: {
     type: String,
-    required: true,
+    // required: true,
   },
   age: {
     type: Number,
