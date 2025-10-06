c75a625309a73bdca42123803901a408852429aa
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-10-06 09:09:36 +0000

    address

--- models/User.js
+++ models/User.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   phone: Number,
+  address : {
+    type: Object
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
