c7b032314d8f5f314b76c20841850b4fce6d69b9
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-11-16 12:50:33 +0000

    mrgin

--- routes/classBooking.js
+++ routes/classBooking.js
@@ -91,7 +91,7 @@ router.get("/student/:id", async (req, res) => {
   try {
     const booking = await ClassBooking.find({
       parent : req.params.id
-    }).populate("mentor", "fullName profilePhoto phone");
+    }).populate("mentor", "fullName profilePhoto phone teachingModes");
     if (!booking)
       return res.status(404).json({ success: false, message: "Not found" });
     res.status(200).json({ success: true, data: booking });
