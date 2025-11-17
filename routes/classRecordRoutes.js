e945d8fd35d3f276504f9b08b5d3e8862f018fab
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-11-17 17:12:13 +0000

    first

--- routes/classRecordRoutes.js
+++ routes/classRecordRoutes.js
@@ -1,3 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const ClassRecord = require("../models/ClassRecord");
+const ClassBooking = require("../models/ClassBooking");
+
+// GET all classRecords
+router.get("/", async (req, res) => {
+  try {
+    const classRecords = await ClassRecord.find()
+    res.status(200).json({ data: classRecords });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+router.get("/class-booking/:id", async (req, res) => {
+  try {
+    const classRecords = await ClassRecord.find({
+        classBooking : req.params.id
+    })
+    res.status(200).json({ data: classRecords });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+router.put("/:id", async (req, res) => {
+  try {
+    const classRecords = await ClassRecord.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).json({ data: classRecords });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 router.post("/", async (req, res) => {
   try {
     console.log(req.body);
@@ -40,3 +72,6 @@ router.post("/", async (req, res) => {
     res.status(500).json({ message: error.message || "Something went wrong" });
   }
 });
+
+
+module.exports = router
