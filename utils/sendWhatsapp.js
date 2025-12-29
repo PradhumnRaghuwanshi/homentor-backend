e625ffa5ca591087c40474c3d54a5585ac9f5965
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-12-29 09:40:22 +0000

    whatsapp

--- utils/sendWhatsapp.js
+++ utils/sendWhatsapp.js
@@ -21,7 +21,7 @@ async function sendWhatsappTemplate({
     whatsapp: {
       messages: [
         {
-          from: "+15557867037", // ✅ NO PLUS SIGN
+          from: "+15557943146", // ✅ NO PLUS SIGN
           to: "919630709988",              // ✅ dynamic number
           content: {
             type: "template",
