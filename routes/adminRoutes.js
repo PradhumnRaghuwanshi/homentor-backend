8359dbeac7b6f08aa03e3171f9dc1afbcc635eda
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2026-01-25 07:15:09 +0000

    fn

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -109,24 +109,28 @@ router.post("/logout", (req, res) => {
 
 router.get("/sidebar-counts", async (req, res) => {
   try {
-    console.log("Counting")
-    const bookings = await ClassBooking.countDocuments({
-      isViewedByAdmin: false,
-    });
-    console.log(bookings)
-    const calls = await CallLog.countDocuments({
-      isViewedByAdmin: false,
-    });
+    // console.log("Counting")
+    // const bookings = await ClassBooking.countDocuments({
+    //   isViewedByAdmin: false,
+    // });
+    // console.log(bookings)
+    // const calls = await CallLog.countDocuments({
+    //   isViewedByAdmin: false,
+    // });
 
-    const mentorRequests = await Mentor.countDocuments({
-      isViewedByAdmin: false,
-    });
+    // const mentorRequests = await Mentor.countDocuments({
+    //   isViewedByAdmin: false,
+    // });
 
-    res.json({
-      bookings,
-      calls,
-      mentorRequests,
-    });
+    // res.json({
+    //   bookings,
+    //   calls,
+    //   mentorRequests,
+    // });
+    const bookings = await ClassBooking.find({
+      isViewedByAdmin: false
+    })
+    res.status(200).json(bookings.length)
   } catch (error) {
     console.error("❌ Sidebar-counts error:", error);
 
